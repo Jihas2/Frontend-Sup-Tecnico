8c949a48c53d01a9b9826833d2bb36e0443bbbd6
jihad <jujughozayel@gmail.com>
2024-10-31 18:21:26 -0300

    Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/models/tecnico.ts
+++ src/app/models/tecnico.ts
@@ -1,9 +1,9 @@
 export class Tecnico{
 
-    constructor(
-      public id: number,
-      public nome: string,
-      public especialidade: string
-    ) {}
+  constructor(
+    public id: number,
+    public nome: string,
+    public especialidade: string
+  ) {}
 
-  }
+}
